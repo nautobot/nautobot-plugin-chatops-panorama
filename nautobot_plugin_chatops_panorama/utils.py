4dec1580c4c8503e561367a11c2c6eb1e4d89c74
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-07-14 08:51:27 -0600

    Finished up remaining tasks for sync-firewalls and formatting is completed.

--- nautobot_plugin_chatops_panorama/utils.py
+++ nautobot_plugin_chatops_panorama/utils.py
@@ -5,7 +5,9 @@ from panos.device import SystemSettings
 from django.utils.text import slugify
 from nautobot.dcim.models import Site, Platform, Manufacturer, DeviceType, Device, DeviceRole, Interface
 from nautobot.extras.models import Status
+from nautobot.ipam.models import IPAddress
 from nautobot_plugin_chatops_panorama.constant import PLUGIN_CFG
+from . import constant
 
 
 def connect_panorama() -> Panorama:
@@ -26,19 +28,33 @@ def _get_or_create_site(site):
         site_obj.save()
     return site_obj
 
-def _get_or_create_platform(platform: str = "PANOS") -> Platform:
+
+def _get_or_create_platform(description: str, platform: str = "PANOS") -> Platform:
     manufacturer_obj = Manufacturer.objects.get(name="Palo Alto Networks")
-    return Platform.objects.get_or_create(name=platform, slug=slugify(platform), manufacturer=manufacturer_obj, napalm_driver="panos")[0]
+    platform = Platform.objects.get_or_create(
+        name=platform,
+        slug=slugify(platform),
+        manufacturer=manufacturer_obj,
+        napalm_driver="panos",
+        description=description,
+    )[0]
+    if platform.description != description:
+        platform.description = description
+        platform.save()
+
+    return platform
+
 
 def _get_or_create_device_type(model: str) -> Platform:
     manufacturer_obj = Manufacturer.objects.get(name="Palo Alto Networks")
     return DeviceType.objects.get_or_create(model=model, slug=slugify(model), manufacturer=manufacturer_obj)[0]
 
-def _get_or_create_device(device: str, serial: str, site: Site, device_type: DeviceType) -> Platform:
+
+def _get_or_create_device(device: str, serial: str, site: Site, device_type: DeviceType, os: str) -> Platform:
     manufacturer_obj = Manufacturer.objects.get(name="Palo Alto Networks")
     device_role_obj = DeviceRole.objects.get(name="Firewall")
     active_status_obj = Status.objects.get(name="Active")
-    device_platform_obj=_get_or_create_platform()
+    device_platform_obj = _get_or_create_platform(description=os)
 
     return Device.objects.get_or_create(
         name=device,
@@ -46,8 +62,28 @@ def _get_or_create_device(device: str, serial: str, site: Site, device_type: Dev
         status=active_status_obj,
         site=site,
         platform=device_platform_obj,
-        device_type=device_type
+        device_type=device_type,
     )[0]
+
+
+def _get_or_create_interfaces(device: Device) -> list[Interface]:
+    """Generate standard interfaces for Palo devices."""
+    interfaces = []
+    for intf in constant.INTERFACES:
+        interfaces.append(Interface.objects.get_or_create(name=intf, device=device, type="1000base-t (ge)")[0])
+
+    return interfaces
+
+
+def _get_or_create_management_ip(device: Device, interface: Interface, ip_address: str):
+    active_status = Status.objects.get(name="Active")
+    mgmt_ip = IPAddress.objects.get_or_create(
+        address=ip_address, status=active_status, assigned_object_id=interface.id
+    )[0]
+    device.primary_ip4 = mgmt_ip
+    device.save()
+
+    return mgmt_ip
 
 
 def _get_group(groups, serial):
@@ -91,7 +127,7 @@ def get_devices(connection: Panorama) -> dict:
         group_name = _get_group(groups_and_devices, device.serial)
         connection.add(device)
         device_system_info = device.show_system_info()["system"]
-#        system_setting = device.find("", SystemSettings)
+        #        system_setting = device.find("", SystemSettings)
         _device_dict[device_system_info["hostname"]] = {
             "hostname": device_system_info["hostname"],
             "serial": device_system_info["serial"],

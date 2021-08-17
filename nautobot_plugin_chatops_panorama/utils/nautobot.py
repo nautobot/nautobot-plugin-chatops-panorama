27338bc2c31c06276544e6aaf89ebe62ca62a2ce
Matt Vitale <matt.vitale@networktocode.com>
2021-08-17 15:45:05 -0400

    Docstrings, logging, formatting fixes

--- nautobot_plugin_chatops_panorama/utils/nautobot.py
+++ nautobot_plugin_chatops_panorama/utils/nautobot.py
@@ -1,93 +1,137 @@
 """Methods for interactions with Panorama."""
 
+import logging
+from typing import List
+
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
 from nautobot.dcim.models import Site, Platform, Manufacturer, DeviceType, Device, DeviceRole, Interface
 from nautobot.extras.models import Status
 from nautobot.ipam.models import IPAddress
-from nautobot_plugin_chatops_panorama.constant import INTERFACES, PANOS_DEVICE_ROLE, PANOS_MANUFACTURER_NAME, NAPALM_DRIVER, PANOS_PLATFORM
+from nautobot_plugin_chatops_panorama.constant import INTERFACES, NAPALM_DRIVER, PANOS_DEVICE_ROLE, PANOS_MANUFACTURER_NAME, PANOS_PLATFORM
 
-from typing import TYPE_CHECKING
 
-# TODO: Fix
-if TYPE_CHECKING or True:
-    from typing import Any, Dict, List, Union
+logger = logging.getLogger("rq.worker")
 
-def _get_or_create_site(site):
+
+def _get_or_create_site(site: str) -> Site:
+    """Gets existing site object from DB, or creates one if not currently present.
+
+    Args:
+        site (str): Name of site.
+
+    Returns:
+        Site: Site object.
+    """    
     active_status = Status.objects.get(name="Active")
     site_obj, created = Site.objects.get_or_create(name=site, slug=slugify(site))
-    if site_obj.status != active_status:
+    if created:
         site_obj.status = active_status
-        site_obj.save()
-    elif created:
         try:
             site_obj.validated_save()
-        except ValidationError:
-            # TODO: Log error here
-            pass
+        except ValidationError as e:
+            logger.error("Error saving newly created site %s: %s", (site, e))
     return site_obj
 
 
-def _get_or_create_platform(description: str, platform: str = PANOS_PLATFORM) -> Platform:
+def _get_or_create_platform(description: str = "", platform: str = PANOS_PLATFORM) -> Platform:
+    """Gets existing platform object from DB, or creates one if not currently present.
+
+    Args:
+        description (str, optional): Platform description. Defaults to "".
+        platform (str, optional): Platform name. Defaults to PANOS_PLATFORM.
+
+    Returns:
+        Platform: Platform object.
+    """    
     manufacturer_obj = Manufacturer.objects.get(name=PANOS_MANUFACTURER_NAME)
     platform_obj, created = Platform.objects.get_or_create(
         name=platform,
-        slug=slugify(platform),
-        manufacturer=manufacturer_obj,
-        napalm_driver=NAPALM_DRIVER,
-        description=description,
+        slug=slugify(platform)
     )
-    if platform_obj.description != description:
+    if created:
+        platform_obj.manufacturer = manufacturer_obj
+        platform_obj.napalm_driver = NAPALM_DRIVER
         platform_obj.description = description
-        platform_obj.save()
-    elif created:
         try:
             platform_obj.validated_save()
-        except ValidationError:
-            # TODO: Log error here
-            pass
+        except ValidationError as e:
+            logger.error("Error saving newly created platform %s: %s", (platform, e))
 
     return platform_obj
 
 
-def _get_or_create_manufacturer(manufacturer_name: str = PANOS_MANUFACTURER_NAME) -> Manufacturer:
-    manufacturer_obj, created = Manufacturer.objects.get_or_create(name=manufacturer_name, slug=slugify(manufacturer_name))
+def _get_or_create_manufacturer(manufacturer: str = PANOS_MANUFACTURER_NAME) -> Manufacturer:
+    """Gets existing manufacturer object from DB, or creates one if not currently present.
+
+    Args:
+        manufacturer (str, optional): Name of manufacturer. Defaults to PANOS_MANUFACTURER_NAME.
+
+    Returns:
+        Manufacturer: Manufacturer object.
+    """    
+    manufacturer_obj, created = Manufacturer.objects.get_or_create(name=manufacturer, slug=slugify(manufacturer))
     if created:
         try:
             manufacturer_obj.validated_save()
-        except ValidationError:
-            # TODO: Log error here
-            pass
+        except ValidationError as e:
+            logger.error("Error saving newly created manufacturer %s: %s", (manufacturer, e))
     return manufacturer_obj
 
 
-def _get_or_create_device_type(model: str) -> Platform:
-    manufacturer_obj = _get_or_create_manufacturer()
+def _get_or_create_device_type(model: str, manufacturer: str = PANOS_MANUFACTURER_NAME) -> DeviceType:
+    """Gets existing device type object from DB, or creates one if not currently present.
+
+    Args:
+        model (str): Model name of device type.
+        manufacturer (str, optional): Name of manufacturer. Defaults to PANOS_MANUFACTURER_NAME.
+
+    Returns:
+        DeviceType: DeviceType object.
+    """    
+    manufacturer_obj = _get_or_create_manufacturer(manufacturer=manufacturer)
     device_type_obj, created = DeviceType.objects.get_or_create(model=model, slug=slugify(model), manufacturer=manufacturer_obj)
     if created:
         try:
             device_type_obj.validated_save()
-        except ValidationError:
-            # TODO: Log error here
-            pass
+        except ValidationError as e:
+            logger.error("Error saving newly created device type %s: %s", (model, e))
     return device_type_obj
 
 
-def _get_or_create_device_role(role_name: str = PANOS_DEVICE_ROLE) -> Platform:
+def _get_or_create_device_role(role_name: str = PANOS_DEVICE_ROLE) -> DeviceRole:
+    """Gets existing device role object from DB, or creates one if not currently present.
+
+    Args:
+        role_name (str, optional): Name of device role. Defaults to PANOS_DEVICE_ROLE.
+
+    Returns:
+        DeviceRole: DeviceRole object.
+    """    
     device_role_obj, created = DeviceRole.objects.get_or_create(name=role_name, slug=slugify(role_name), color="red")
     if created:
         try:
             device_role_obj.validated_save()
-        except ValidationError:
-            # TODO: Log error here
-            pass
+        except ValidationError as e:
+            logger.error("Error saving newly created device role %s: %s", (role_name, e))
     return device_role_obj
 
 
-def _get_or_create_device(device: str, serial: str, site: str, device_type: DeviceType, os: str) -> Platform:
+def _get_or_create_device(device: str, site: str, device_type: DeviceType, serial: str = "", os_description: str = "") -> Device:
+    """Gets existing device object from DB, or creates one if not currently present.
+
+    Args:
+        device (str): Name of device.
+        site (str): Name of site.
+        device_type (DeviceType): DeviceType object.
+        serial (str, optional): Device serial number. Defaults to "".
+        os_description (str, optional): Description of OS. Defaults to "".
+
+    Returns:
+        Device: Device object.
+    """    
     device_role_obj = _get_or_create_device_role()
     active_status_obj = Status.objects.get(name="Active")
-    device_platform_obj = _get_or_create_platform(description=os)
     site_obj = _get_or_create_site(site)
 
     device_obj, created = Device.objects.get_or_create(
@@ -95,66 +139,79 @@ def _get_or_create_device(device: str, serial: str, site: str, device_type: Devi
         device_role=device_role_obj,
         status=active_status_obj,
         site=site_obj,
-        platform=device_platform_obj,
-        device_type=device_type,
-        serial=serial
+        device_type=device_type
     )
     if created:
+        device_obj.platform = _get_or_create_platform(description=os_description)
+        if serial:
+            device_obj.serial = serial
         try:
             device_obj.validated_save()
-        except ValidationError:
-            # TODO: Log error here
-            pass
-    # TODO: Work on this
+        except ValidationError as e:
+            logger.error("Error saving newly created device %s: %s", (device, e))
+    # TODO: Figure this out later
     # device_obj.custom_field_data['public_ipv4'] = '3.13.252.97'
     # device_obj.save()
 
     return device_obj
 
 
+# TODO: This function needs to dynamically create the interfaces.
 def _get_or_create_interfaces(device: Device) -> List[Interface]:
-    """Generate standard interfaces for Palo devices."""
+    """Generate standard interfaces for Palo devices.
+
+    Args:
+        device (Device): Device object to create interfaces for.
+
+    Returns:
+        List[Interface]: List of Interface objects created.
+    """    
     interfaces = []
     for interface in INTERFACES:
         interface_obj, created = Interface.objects.get_or_create(name=interface, device=device, type="1000base-t (ge)")
         if created:
             try:
                 interface_obj.validated_save()
-            except ValidationError:
-                # TODO: Log error here
-                pass
+            except ValidationError as e:
+                logger.error("Error saving newly created interface %s: %s", (interface, e))
         interfaces.append(interface_obj)
 
     return interfaces
 
 
-def _get_or_create_management_ip(device: Device, interfaces: List[Interface], ip_address: str):
-    """Get or create management IP for an interface on a device."""
+def _get_or_create_management_ip(device: Device, interfaces: List[Interface], ip_address: str) -> IPAddress:
+    """Get or create management IP and assign to Management interface on device.
+
+    Args:
+        device (Device): Device object to assign IP to.
+        interfaces (List[Interface]): List of all interfaces for device.
+        ip_address (str): IP address in CIDR notation to create and assign.
+
+    Returns:
+        IPAddress: IPAddress object.
+    """    
     active_status = Status.objects.get(name="Active")
     interface_obj = [interface for interface in interfaces if interface.name.startswith("Management")][0]
     mgmt_ip_obj, created = IPAddress.objects.get_or_create(
         address=ip_address, status=active_status, assigned_object_id=interface_obj.id
     )
-    if created:
-        try:
-            mgmt_ip_obj.validated_save()
-        except ValidationError:
-            # TODO: Log e
-            pass
 
     mgmt_ip_obj.assigned_object_id = interface_obj.id
     mgmt_ip_obj.assigned_object_type_id = 37
+
     try:
         mgmt_ip_obj.validated_save()
-    except ValidationError:
-        # TODO: Log e
-        pass
+    except ValidationError as e:
+        if created:
+            logger.error("Error saving newly created management IP %s: %s", (ip_address, e))
+        else:
+            logger.error("Error saving info for management IP %s: %s", (ip_address, e))
 
     device.primary_ip4_id = mgmt_ip_obj.id
+    
     try:
         device.validated_save()
-    except ValidationError:
-        # TODO: Log e
-        pass
+    except ValidationError as e:
+        logger.error("Error saving management IP %s to device %s: %s", (ip_address, device.name, e))
 
     return mgmt_ip_obj

036210a32e07453512d52c9aca7b0d7e34adafbd
Justin Drew <jdrew82@users.noreply.github.com>
2022-12-14 11:28:54 -0600

    test: ✅ Add test for get_devicegroups_from_pano function

--- nautobot_plugin_chatops_panorama/tests/test_utils_panorama.py
+++ nautobot_plugin_chatops_panorama/tests/test_utils_panorama.py
@@ -1,7 +1,7 @@
 """Unit tests for Panorama utility functions."""
 from unittest.mock import MagicMock
 from nautobot.utilities.testing import TestCase
-from nautobot_plugin_chatops_panorama.utils.panorama import get_devices_from_pano
+from nautobot_plugin_chatops_panorama.utils.panorama import get_devices_from_pano, get_devicegroups_from_pano
 
 
 class TestPanoramaUtils(TestCase):
@@ -42,4 +42,31 @@ class TestPanoramaUtils(TestCase):
             }
         }
         result = get_devices_from_pano(connection=mock_conn)
+        self.assertEqual(result, expected)
+
+    def test_get_devicegroups_from_pano(self):
+        """Test the get_devicegroups_from_pano function success."""
+        mock_device = MagicMock()
+        mock_device.show_system_info = MagicMock()
+        mock_device.show_system_info.return_value = {
+            "system": {
+                "hostname": "Test",
+                "serial": 123456,
+                "ip-address": "1.1.1.1/32",
+                "model": "PAN-2110",
+                "sw-version": "10.0.2",
+            }
+        }
+        mock_group = MagicMock()
+        mock_group.name = "Test Group"
+        mock_group.children = [mock_device]
+        mock_conn = MagicMock()
+        mock_conn.refresh_devices = MagicMock()
+        mock_conn.refresh_devices.return_value = [mock_group]
+        expected = {
+            "Test Group": {
+                "devices": ["Hostname: Test\nAddress: 1.1.1.1/32\nSerial: 123456\nModel: PAN-2110\nVersion: 10.0.2\n\n"]
+            }
+        }
+        result = get_devicegroups_from_pano(connection=mock_conn)
         self.assertEqual(result, expected)
